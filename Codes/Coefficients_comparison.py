ef1fc112243c8a13cc0a95ccb7a47f982d6e5bb3
samuel321d <samuel.prieto@udea.edu.co>
2026-06-01 15:52:53 -0500

    c

--- Codes/Coefficients_comparison.py
+++ Codes/Coefficients_comparison.py
@@ -18,25 +18,41 @@ plt.rcParams.update({
     #"font.size": 12,
     "axes.labelsize": 16,
     "axes.titlesize": 18,
-    "legend.fontsize": 12,
+    "legend.fontsize": 15,
     "xtick.labelsize": 12,
     "ytick.labelsize": 12,
     "lines.linewidth": 2
 })
 
 
-cd_m_09, cy_m_09 = Values.values(0.9)
+cd_m_09, cy_m_09 = Values.values(0.95)
 cd_m_08, cy_m_08 = Values.values(0.8)
 cd_m_06, cy_m_06 = Values.values(0.6)
 
-configs = np.array(1, 15, 1)
+configs = np.linspace(1, 15, 15)
 
 plt.figure(figsize=(10,8))
 plt.plot(configs, cd_m_08, linestyle = "--", marker = "s", label = "$C_D$ for $M = 0.8$")
 plt.plot(configs, cd_m_06, linestyle = "-.", marker = "^", label = "$C_D$ for $M = 0.6$")
-plt.plot(configs, cd_m_09, linestyle = "-.", marker = "o", label = "$C_D$ for $M = 0.95$")
+#plt.plot(configs, cd_m_09, linestyle = "-.", marker = "o", label = "$C_D$ for $M = 0.95$")
 plt.grid(True, linestyle = "--")
 plt.legend()
 plt.title("$C_D$ comparison for various Mach numbers and configurations")
+plt.xlabel("Configuration")
+plt.ylabel("$C_D$")
+plt.yticks(np.arange(0.7, 0.78, 0.01))
+plt.xticks(np.arange(1, 16, 1))
+plt.show()
+
+plt.figure(figsize=(10,8))
+plt.plot(configs, cy_m_08, linestyle = "--", marker = "s", label = "$C_Y$ for $M = 0.8$")
+plt.plot(configs, cy_m_06, linestyle = "-.", marker = "^", label = "$C_Y$ for $M = 0.6$")
+#plt.plot(configs, cy_m_09, linestyle = "-.", marker = "o", label = "$C_Y$ for $M = 0.95$")
+plt.grid(True, linestyle = "--")
+plt.legend()
+plt.title("$C_Y$ comparison for various Mach numbers and configurations")
+plt.xlabel("Configuration")
+plt.ylabel("$C_Y$")
+plt.xticks(np.arange(1, 16, 1))
 plt.show()
 
