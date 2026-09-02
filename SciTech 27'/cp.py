874f78f41f90a08772c5dfcc898ca311d1d9bac9
Marcos <marcos.cardona@udea.edu.co>
2026-09-01 23:22:16 -0500

    add potential perturbation code

--- SciTech 27'/cp.py
+++ SciTech 27'/cp.py
@@ -1,11 +1,26 @@
+# Cp from linearized supersonic theory
+
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
 
-# Generate example
-x = np.arange(0, 2*np.pi, 0.1)
-sinx = np.sin(x)
+# Generate curve
+x = np.arange(0, np.pi, 0.01)
+curve = 0.1 * np.sin(x)
+
+# Derivative
+slope = np.gradient(curve, x)
+theta = np.arctan(slope)
+
+M_infty = 1.4
+
+gamma = np.sqrt(M_infty**2 - 1)
+
+cp = 2 * theta / gamma
 
 plt.figure(figsize = (10, 5))
-plt.plot(x, sinx)
+plt.plot(x, curve)
+plt.plot(x, cp)
 plt.grid()
-plt.show()
+plt.savefig('grafica.png')
